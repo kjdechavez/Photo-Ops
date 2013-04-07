b1896b97b1f33406ee1b2c374e369a1b5a907ace
vaanxii <jemeelsblackblade@yahoo.com>
2013-04-07 14:24:24 +0800

    Context of enties as a collection.

---
+++ MvcPhotoOps/Models/Entities.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcPhotoOps.Models
+{
+    public class Entities
+    {
+        public List<Event> Events { get; set; }
+    }
+}
