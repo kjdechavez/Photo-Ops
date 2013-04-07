90f5914f6ca3aecc88f43a1327d747bfd9d8c55a
vaanxii <jemeelsblackblade@yahoo.com>
2013-04-07 14:22:28 +0800

    Event domain model.

---
+++ MvcPhotoOps/Models/Event.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MvcPhotoOps.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+}
